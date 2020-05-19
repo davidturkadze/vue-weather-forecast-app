9ad5006023d21f6a8eb173e60fa98ef0a644499a
David Turkadze <nebel89@gmail.com>
2020-05-19 19:56:32 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>This component’s code is in {{ filename }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      filename: 'App.vue'
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './assets/css/style.css';
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import Widgets from 'fusioncharts/fusioncharts.widgets';
+import PowerCharts from 'fusioncharts/fusioncharts.powercharts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+import VueFusionCharts from 'vue-fusioncharts';
+
+Charts(FusionCharts);
+PowerCharts(FusionCharts);
+Widgets(FusionCharts);
+FusionTheme(FusionCharts);
+
+// Global registration of components for project-wide use
+Vue.use(VueFusionCharts, FusionCharts);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  store,
-  render: h => h(App)
+    store,
+    render: h => h(App)
 }).$mount("#app");
